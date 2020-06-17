86ae1936822ee8eeda28620708817a0dc84de24e
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-06-17 16:09:00 -0300

    Global Style added

---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+/**
+ * COLOR PALLETE
+ * #031D44
+ * #04395E
+ * #72B01D
+ * #F8F7F9
+ * #F8333C
+ */
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #f8f7f9;
+  }
+`;
