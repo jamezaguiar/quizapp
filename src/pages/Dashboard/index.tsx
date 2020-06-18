10c24e4de743e40762ff5308a05263901fbcc27a
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2020-06-18 14:43:06 -0300

    Dashboard (initial) page finalized

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
-};
+import Header from '../../components/Header';
+
+import logo from '../../assets/question-logo.svg';
+
+import { Verse, VerseContainer, DashboardContainer } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <Header centralized />
+    <DashboardContainer>
+      <VerseContainer>
+        <Verse>
+          &quot;Os sábios acumulam
+          <br />
+          conhecimento...&quot;
+          <br />
+          <span>- Provérbios 10:14a</span>
+        </Verse>
+        <div>
+          <button type="button">Iniciar Quiz</button>
+          <span>Ver resultados anteriores</span>
+        </div>
+      </VerseContainer>
+      <img src={logo} alt="logo" />
+    </DashboardContainer>
+  </>
+);
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,61 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  font-size: 48px;
-  color: #3a3a3a;
+export const DashboardContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    width: 275px;
+    margin-top: 25px;
+    margin-right: 100px;
+  }
+`;
+
+export const VerseContainer = styled.div`
+  max-width: 600px;
+
+  margin-left: 100px;
+
+  div {
+    margin-top: 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    button {
+      width: 175px;
+      height: 50px;
+
+      font-family: 'Josefin Sans', Roboto, sans-serif;
+      font-weight: 400;
+      font-size: 16pt;
+
+      border: none;
+      border-radius: 4px;
+      background: #dd0426;
+      color: #f2f5ea;
+    }
+
+    span {
+      font-family: 'Josefin Sans', Roboto, sans-serif;
+      font-weight: 400;
+      font-size: 16pt;
+      color: #dd0426;
+      cursor: pointer;
+    }
+  }
+`;
+
+export const Verse = styled.h1`
+  font-family: 'Josefin Sans', Roboto, sans-serif;
+  font-size: 60px;
+  font-weight: 400;
+  font-style: italic;
+  color: #050532;
+
+  span {
+    font-size: 48px;
+    font-weight: 300;
+  }
 `;
